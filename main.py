0c5d042421eb6ddcc154f36d124596bbf111a9fc
Giuseppe Tedesco <gt0488702@gmail.com>
2024-11-15 11:40:46 +0100

    Aggiunto un punto

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/prova')
 def prova():
-    return '<h1>Pagina di prova</h1>'
+    return '<h1>Pagina di prova.</h1>'
 
 
 app.run(host='localhost', port=3000, debug=True)
